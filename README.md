9502ac409047a19be7624154d6932f0eab62e4db
Samuel Raphael <samueloraphael@gmail.com>
2023-10-13 01:49:21 +0100

    update endpoint definitions and readme according to rubric

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # gostumer
-My submission for Udacity's Go course final project
+
+Gostumer is a Customer Relationship Management API that handles the information
+of onboarded customers.
+
+## Run The Application
+To run the application, you need to have at least go.19 installed.
+
+- Open the project in your favorite editor
+- Run `go mod download` to install dependencies
+- Start the application using `go run main.go`
+- Make requests to the endpoints through Postman or cURL
+
+## Endpoints
+- Get all customers: `GET /api/v1/customers`
+- Get a single customer: `GET /api/v1/customers/{id}`
+- Update a single customer: `PUT /api/v1/customers/{id}`
+- Add a new customer: `POST /api/v1/customers`
+- Delete a customer: `DELETE /api/v1/customers/{id}`
--- main.go
+++ main.go
@@ -14,11 +14,11 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/customers", customers.GetCustomers).Methods("GET")
-	router.HandleFunc("/customers", customers.CreateCustomer).Methods("POST")
-	router.HandleFunc("/customers/{id}", customers.GetSingleCustomer).Methods("GET")
-	router.HandleFunc("/customers/{id}", customers.UpdateCustomer).Methods("PUT")
-	router.HandleFunc("/customers/{id}", customers.DeleteCustomer).Methods("DELETE")
+	router.HandleFunc("/api/v1/customers", customers.GetCustomers).Methods("GET")
+	router.HandleFunc("/api/v1/customers", customers.CreateCustomer).Methods("POST")
+	router.HandleFunc("/api/v1/customers/{id}", customers.GetSingleCustomer).Methods("GET")
+	router.HandleFunc("/api/v1/customers/{id}", customers.UpdateCustomer).Methods("PUT")
+	router.HandleFunc("/api/v1/customers/{id}", customers.DeleteCustomer).Methods("DELETE")
 
 	fmt.Println("Server is running on port 8000")
 	err := http.ListenAndServe(":8000", handlers.LoggingHandler(os.Stdout, router))
